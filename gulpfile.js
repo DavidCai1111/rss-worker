93f0c919fbdb30955c00558ca93fb4f967e6a9c1
DavidCai <376462191@qq.com>
2015-05-07 18:03:35 +0800

    测试

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,20 @@ gulp.task('compile_coffee', function () {
 });
 
 gulp.task('test', ['compile_coffee'], function (cb) {
-  gulp.src(['build/lib/**/*.js', 'build/index.js'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .on('finish', function () {
-      gulp.src(['build/test/*.js'])
-        .pipe(mocha())
-        .pipe(istanbul.writeReports())
-        .on('end', cb)
-    })
+  //确保coffee已被编译
+  setTimeout(test, 1000 * 5);
+
+  function test() {
+    gulp.src(['build/lib/**/*.js', 'build/index.js'])
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire())
+      .on('finish', function () {
+        gulp.src(['build/test/*.js'])
+          .pipe(mocha())
+          .pipe(istanbul.writeReports())
+          .on('end', cb)
+      })
+  }
 });
 
 gulp.task('default', ['compile_coffee'], function () {
